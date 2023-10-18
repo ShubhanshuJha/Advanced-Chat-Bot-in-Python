8e6d84e85b9dd3e5a1e03b9b288762984b6b8b5b
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-19 01:31:11 +0530

    making a summary of the entire content with curated prompt

--- model.py
+++ model.py
@@ -1,25 +1,43 @@
 # import os
 import openai
 from langchain.utilities import WikipediaAPIWrapper
+from langchain import OpenAI
+from langchain import PromptTemplate
+import os 
+from dotenv import find_dotenv, load_dotenv
 
-with open("API_KEY", 'r') as f:
-    openai.api_key = f.read().strip()
+# with open("API_KEY", "r") as f:
+#     openai.api_key = f.read().strip()
 MODEL_ENGINE = "text-davinci-002"
 
+load_dotenv(find_dotenv())
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 question = "who is naruto"
-def wiki_answer(question):
-    wiki= WikipediaAPIWrapper()
-    result = wiki.run(question)
-    
 
+
+def wiki_answer(question):
+    wiki = WikipediaAPIWrapper()
+
+    llm = OpenAI(temperature=0.5)
+    result = wiki.run(question)
+    template = """
+    Please write a concise and to the point summary, make it easy to understand, include all important points from the following text:
+    {result}
+    Write only those points whhich are related to the {question}
+"""
+    prompt = PromptTemplate(input_variables=["result","question"], template=template)
+    summary_prompt = prompt.format(result=result,question=question)
+    summary = llm(summary_prompt)
+
+    return summary
 
 
 print(wiki_answer(question))
 
+
 def generate_answer(question):
-    prompt = (f"Question: {question}\n"
-              "Answer:")
+    prompt = f"Question: {question}\n" "Answer:"
     completions = openai.Completion.create(
         engine=MODEL_ENGINE,
         prompt=prompt,
