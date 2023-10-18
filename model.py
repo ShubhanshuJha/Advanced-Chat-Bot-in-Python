782ace2878c22f6b51382cec58a1867b4f911fcf
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-19 01:15:12 +0530

    for retrieving wikipedia content

--- model.py
+++ model.py
@@ -1,10 +1,21 @@
 # import os
 import openai
+from langchain.utilities import WikipediaAPIWrapper
 
 with open("API_KEY", 'r') as f:
     openai.api_key = f.read().strip()
 MODEL_ENGINE = "text-davinci-002"
 
+
+question = "who is naruto"
+def wiki_answer(question):
+    wiki= WikipediaAPIWrapper()
+    result = wiki.run(question)
+    
+
+
+
+print(wiki_answer(question))
 
 def generate_answer(question):
     prompt = (f"Question: {question}\n"
